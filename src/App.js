777defa3bfae555257c692deb8177d32b387268d
wrathofsalim <98185821+wrathofsalim@users.noreply.github.com>
2023-03-10 18:01:06 +0200

    d

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Enemies from './Components/Enemies';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container p-2 m-2 border border-3 border-secondary rounded text-light'>
+        <h2>DPS MASTER</h2>
+        <div className='col'>
+          <Enemies id="1"/>
+        </div>
     </div>
-  );
+    );
 }
 
 export default App;
